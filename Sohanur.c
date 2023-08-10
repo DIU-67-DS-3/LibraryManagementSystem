15fc812c53be4d1cce1056fd677472a8c6f2343e
S. M Moshaddaque Hoshain <100718549+moshaddaque@users.noreply.github.com>
2023-08-10 22:46:45 +0600

    Create Searching function
    Create Searching function using book name and author name..

--- Sohanur.c
+++ Sohanur.c
@@ -1,6 +1,18 @@
-#include<stdio.h>
-int main()
+// Function to search for a book by title or author
+void searchBook(struct Book library[], int num_books, char query[])
 {
-    printf("HI This is SOHANUR");
-    return 0;
+    int found = 0;
+    for (int i = 0; i < num_books; i++)
+    {
+        if (strstr(library[i].title, query) || strstr(library[i].author, query))
+        {
+            printf("\n\t\t\t\t\033[1;34mTitle: %s\033[0m\n\t\t\t\tAuthor: %s\n\t\t\t\tDepartment: %s\n\t\t\t\tISBN: %s\n\t\t\t\tAvailability: %s\n\n",
+                   library[i].title, library[i].author, library[i].department, library[i].isbn, library[i].is_available ? "Available" : "Not Available");
+            found = 1;
+        }
+    }
+    if (!found)
+    {
+        printf("\n\t\t\t\t\033[1;31mBook not found!\033[0m\n");
+    }
 }
